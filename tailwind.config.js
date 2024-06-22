a7c6910f9a7eeb85af9bfdb865812b66c540d216
Grentenville <arturtrentinibenedetti@gmail.com>
2024-06-22 00:07:00 -0300

    Recursos base

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
         'surfaceInverted':'#2e2e2e',//Background Inverted
         'onSurface':'#D1D1D1',//Text on Background
         'surfaceContainerLow':'#D1D1D1',//Container on background
-        'surfaceContainer':'#a7a7a7',//Container on background
+        'surfaceContainer':'#1e1e1e',//Container on background
         'surfaceContainerHigh':'#D1D1D1',//Container on background
         'primary':'#ffe3f4',//Primary Color
         'onPrimary':'#F2F2F2',//Text on Primary Color
