7df889612348b61fe9f73428e6e76a75fb33abae
Grentenville <arturtrentinibenedetti@gmail.com>
2024-06-21 21:54:40 -0300

    Initial commit (I forgor)

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+// npm install -D tailwindcss
+//npx tailwindcss init
+//npx tailwindcss -i ./css/input.css -o ./css/style.css --watch
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'surface':'#121212',//Background
+        'surfaceInverted':'#2e2e2e',//Background Inverted
+        'onSurface':'#D1D1D1',//Text on Background
+        'surfaceContainerLow':'#D1D1D1',//Container on background
+        'surfaceContainer':'#a7a7a7',//Container on background
+        'surfaceContainerHigh':'#D1D1D1',//Container on background
+        'primary':'#ffe3f4',//Primary Color
+        'onPrimary':'#F2F2F2',//Text on Primary Color
+        'secondary':'#fff0f5',//Yada Yada...
+        'onSecondary':'#1A1A1A',
+        'tertiary':'#E34234',
+        //'onTertiary':'#F2F2F2',
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
+
