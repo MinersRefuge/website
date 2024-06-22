a5774e38c04cbbbb1c6135283d3573006e0d7234
Grentenville <arturtrentinibenedetti@gmail.com>
2024-06-22 16:13:12 -0300

    Mudança de cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'surface':'#121212',//Background
+        'surface':'#0F172A',//Background
         'surfaceInverted':'#2e2e2e',//Background Inverted
         'onSurface':'#D1D1D1',//Text on Background
         'surfaceContainerLow':'#D1D1D1',//Container on background
-        'surfaceContainer':'#1e1e1e',//Container on background
+        'surfaceContainer':'#1E293B',//Container on background
         'surfaceContainerHigh':'#D1D1D1',//Container on background
         'primary':'#ffe3f4',//Primary Color
         'onPrimary':'#F2F2F2',//Text on Primary Color
