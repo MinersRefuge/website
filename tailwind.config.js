00472697aa99d7bf0c1ee587009128161505b863
Grentenville <arturtrentinibenedetti@gmail.com>
2024-06-22 16:51:22 -0300

    Comment unused colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'surface':'#0F172A',//Background
+        'surface':'#0F172A',//Background - slate-900
         'surfaceInverted':'#2e2e2e',//Background Inverted
         'onSurface':'#D1D1D1',//Text on Background
-        'surfaceContainerLow':'#D1D1D1',//Container on background
-        'surfaceContainer':'#1E293B',//Container on background
-        'surfaceContainerHigh':'#D1D1D1',//Container on background
+        //'surfaceContainerLow':'#D1D1D1',//Container on background
+        'surfaceContainer':'#1E293B',//Container on background - slate-800
+        //'surfaceContainerHigh':'#D1D1D1',//Container on background
         'primary':'#ffe3f4',//Primary Color
-        'onPrimary':'#F2F2F2',//Text on Primary Color
+        //'onPrimary':'#F2F2F2',//Text on Primary Color
         'secondary':'#fff0f5',//Yada Yada...
-        'onSecondary':'#1A1A1A',
+        //'onSecondary':'#1A1A1A',
         'tertiary':'#f38fcb',
         //'onTertiary':'#F2F2F2',
       },
